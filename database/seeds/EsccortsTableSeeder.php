633bafa009dbb1d584d9d397f4405f44a7f7e412
argadhana <yoshspecta333@gmail.com>
2020-09-07 12:58:45 +0700

    pembetulan seeder esccort

--- database/seeds/EsccortsTableSeeder.php
+++ database/seeds/EsccortsTableSeeder.php
@@ -12,13 +12,24 @@ class EsccortsTableSeeder extends Seeder
     public function run()
     {
         DB::table('esccorts')->insert([
-            'salary' => 'esccort1',
+            'salary' => '40000',
             'keahlian' => 'mencuci',
             'name' => 'okegays',
             'age' => 22,
             'address' => 'jalanmanaaja',
             'gender' => 'laki-laki',
             'phone' => 86282,
+            'rating' => 4,
+        ]);
+
+        DB::table('esccorts')->insert([
+            'salary' => '40000',
+            'keahlian' => 'memasak',
+            'name' => 'kamuiyakamu',
+            'age' => 18,
+            'address' => 'pentingnyaman',
+            'gender' => 'perempuan',
+            'phone' => 869878282,
             'rating' => 4,
         ]);
     }
